8a0de1cbe9e8b479b161c725ea5f1c36590362b3
nirmalmazhuvanpilly <nirquarous@gmail.com>
2021-06-05 21:28:21 +0530

    theme changed to red

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.red,
         ),
         home: HomePage(),
       ),
